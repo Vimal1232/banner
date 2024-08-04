c77bc9ed7402e42cf4d0cffb9983e0db8aefeb9c
Vimal Chaudhary <86522515+Vimal1232@users.noreply.github.com>
2024-08-05 01:31:58 +0530

    again change

--- components/Mainhead.jsx
+++ components/Mainhead.jsx
@@ -35,7 +35,7 @@ const Mainhead = () => {
   const [Id, setId] = useState(0);
   const [Image, setimage] = useState("");
   const [clickedImage, setClickedImage] = useState(null);
-  const [BannerData, setBannerData] = useState();
+  const [BannerData, setBannerData] = useState(BannerDatafromfile);
 
   const UpdateData = async (e) => {
     e.preventDefault();
